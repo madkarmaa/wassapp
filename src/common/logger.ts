0e5b29364fe9f99a33aba592b96b4592fe9b0e22
madkarmaa <madkarma99@gmail.com>
2026-01-31 20:46:30 +0100

    refactor: don't stringify logger args

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -11,32 +11,29 @@ export enum LogLevel {
 let logLevel: LogLevel = LogLevel.INFO;
 export const setLogLevel = (level: LogLevel) => (logLevel = level);
 
-const craftMessage = (level: LogLevel, tags: string[], ...args: unknown[]) => {
+const craftMessage = (level: LogLevel, tags: string[]) => {
     const levelStr = LogLevel[level] as keyof typeof LogLevel;
     const timestamp = new Date().toISOString().replace('T', ' ').replace('Z', '');
     const tagStr = tags
         .map((tag) => tag.trim())
         .filter((tag) => tag)
         .join('/');
-    const message = args
-        .map((arg) => (typeof arg === 'string' ? arg : JSON.stringify(arg)))
-        .join(' ');
 
-    return `${timestamp}  ${levelStr}  --- [${APP_NAME}/${tagStr}] ${message}`;
+    return `${timestamp}  ${levelStr}  --- [${APP_NAME}/${tagStr}]`;
 };
 
 export const taggedLogger = (...tags: string[]) => {
     return {
         error: (...args) =>
-            logLevel >= LogLevel.ERROR && console.log(craftMessage(LogLevel.ERROR, tags, ...args)),
+            logLevel >= LogLevel.ERROR && console.log(craftMessage(LogLevel.ERROR, tags), ...args),
         warn: (...args) =>
-            logLevel >= LogLevel.WARN && console.log(craftMessage(LogLevel.WARN, tags, ...args)),
+            logLevel >= LogLevel.WARN && console.log(craftMessage(LogLevel.WARN, tags), ...args),
         info: (...args) =>
-            logLevel >= LogLevel.INFO && console.log(craftMessage(LogLevel.INFO, tags, ...args)),
+            logLevel >= LogLevel.INFO && console.log(craftMessage(LogLevel.INFO, tags), ...args),
         debug: (...args) =>
-            logLevel >= LogLevel.DEBUG && console.log(craftMessage(LogLevel.DEBUG, tags, ...args)),
+            logLevel >= LogLevel.DEBUG && console.log(craftMessage(LogLevel.DEBUG, tags), ...args),
         verbose: (...args) =>
             logLevel >= LogLevel.VERBOSE &&
-            console.log(craftMessage(LogLevel.VERBOSE, tags, ...args))
+            console.log(craftMessage(LogLevel.VERBOSE, tags), ...args)
     } satisfies Record<Lowercase<keyof typeof LogLevel>, (...args: unknown[]) => void>;
 };
